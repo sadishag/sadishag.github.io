10861666460fed5d361f7bc69587dd7ab0de3ef2
Sadisha Galappatti <sadishag@gmail.com>
2018-12-08 19:28:06 -0500

    Adding routing and configured for home, about, and contact

--- personalsite/src/app/app-routing.module.ts
+++ personalsite/src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeBodyComponent } from './home-body/home-body.component'
+import { AboutBodyComponent } from './about-body/about-body.component'
+import { ContactBodyComponent } from './contact-body/contact-body.component'
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeBodyComponent }
+  { path: 'home', component: HomeBodyComponent },
+  { path: 'about', component: AboutBodyComponent },
+  { path: 'contact', component: ContactBodyComponent }
 ]
 
 @NgModule({
